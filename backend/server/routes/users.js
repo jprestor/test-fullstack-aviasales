4872897ba4da887021f3fefd62048b62d2d79907
Dmitriy Miroshnichenko <d.mir.posts@gmail.com>
2023-02-08 16:44:17 +0500

    add backends

--- backend/server/routes/users.js
+++ backend/server/routes/users.js
@@ -1,7 +1,5 @@
 // @ts-check
 
-import i18next from 'i18next';
-
 export default (app) => {
   app
     .get('/users', { name: 'users' }, async (req, reply) => {
@@ -18,12 +16,14 @@ export default (app) => {
       user.$set(req.body.data);
 
       try {
-        const validUser = await app.objection.models.user.fromJson(req.body.data);
+        const validUser = await app.objection.models.user.fromJson(
+          req.body.data
+        );
         await app.objection.models.user.query().insert(validUser);
-        req.flash('info', i18next.t('flash.users.create.success'));
+        req.flash('info', 'some_message');
         reply.redirect(app.reverse('root'));
       } catch ({ data }) {
-        req.flash('error', i18next.t('flash.users.create.error'));
+        req.flash('error', 'some_message');
         reply.render('users/new', { user, errors: data });
       }
 
