8e51cc1e58a016849bb57e9b78c09b21c975a053
Dmitriy Miroshnichenko <d.mir.posts@gmail.com>
2023-02-14 22:49:26 +0500

    [backend] fix vercel starter

--- backend/api/serverless.js
+++ backend/api/serverless.js
@@ -10,7 +10,7 @@ const app = Fastify({
 });
 
 // Register your application as a normal plugin.
-app.register(import('../src/app'));
+app.register(import('../src/app.js'));
 
 export default async (req, res) => {
   await app.ready();
