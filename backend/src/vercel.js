5af74f91d833e07ccc46cb53db373596ee0cd761
Dmitriy Miroshnichenko <d.mir.posts@gmail.com>
2023-02-14 21:44:03 +0500

    [backend] fix vercel starter

--- backend/src/vercel.js
+++ backend/src/vercel.js
@@ -1,15 +1,15 @@
 import dotenv from 'dotenv';
 import Fastify from 'fastify';
+import app from './app.js';
 
 dotenv.config();
-const Fastify = require('');
 
 // Instantiate Fastify with some config
-const app = Fastify({ logger: true });
+const vercelApp = Fastify({ logger: true });
 // Register your application as a normal plugin.
-app.register(require('./app.js'));
+vercelApp.register(app);
 
-module.exports = async (req, res) => {
-  await app.ready();
-  app.server.emit('request', req, res);
+export default async (req, res) => {
+  await vercelApp.ready();
+  vercelApp.server.emit('request', req, res);
 };
