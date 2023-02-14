b271592ff23f02dc2429b3b1e17d31ec5edd967e
Dmitriy Miroshnichenko <d.mir.posts@gmail.com>
2023-02-14 17:19:43 +0500

    fix frontend env variables/add versel.json for backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import { Home, Success } from '@/routes';
 
 const App = () => {
   return (
-    <div className="flex flex-col min-h-screen bg-[url(/back.jpg)] bg-no-repeat bg-top">
+    <div className="flex flex-col min-h-screen bg-[url(/back.jpg)] bg-cover bg-top">
       <div className="container py-[110px] sm:py-[50px]">
         <Link className="block mb-[82px] sm:mb-[55px]" to="me">
           <img src="/logo.svg" alt="Aviasales logo" />
