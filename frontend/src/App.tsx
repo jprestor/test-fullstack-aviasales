5ff6cfe50b54c297ccb05206a1366e0c84858960
Dmitriy Miroshnichenko <d.mir.posts@gmail.com>
2023-02-19 20:46:07 +0500

    [frontend] fix logo link

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,7 +6,7 @@ const App = () => {
   return (
     <div className="flex min-h-screen flex-col bg-[url(/back.jpg)] bg-cover bg-top">
       <div className="container pt-[11.5vh] pb-[40px] sm:py-[50px]">
-        <Link className="mb-[9vh] block sm:mb-[55px]" to="me">
+        <Link className="mb-[9vh] block sm:mb-[55px]" to="/">
           <img className="w-[15.9vh]" src="/logo.svg" alt="Aviasales logo" />
         </Link>
 
